204c4b595f0bafcdcfc75142966eecf039c90797
sheetalkashid18 <sheetalsk1998@gmail.com>
2022-06-26 18:05:02 -0400

    commit now

--- Start/Overview/DataTypes/Program.cs
+++ Start/Overview/DataTypes/Program.cs
@@ -9,14 +9,19 @@ decimal d = 400.85m;
 int b = default;
 bool tf = default;
 
-Console.WriteLine($"{a}, {b}, {tf}, {c}, {f}, {d}");
+// Console.WriteLine($"{a}, {b}, {tf}, {c}, {f}, {d}");
 
-// TODO: implicit type conversion
+// // TODO: implicit type conversion
+// Console.WriteLine($"{c + a}");
+// Console.WriteLine($"{(char)(c + a)}");
+// Console.WriteLine($"{f + a}");
+// Console.WriteLine($"{f + c}");
 
 
 // TODO: Create an instance of a struct (which is a value type)
-
-
+s s1;
+s1.a = 5;
+s1.b = false;
 // Perform an operation on a struct
 void StructOp(s theStruct) {
     // Modify the struct properties inside the function
@@ -25,13 +30,13 @@ void StructOp(s theStruct) {
     Console.WriteLine($"{theStruct.a}, {theStruct.b}");
 }
 
-// Console.WriteLine("Structs are passed by copy, since they are value types:");
-// Console.WriteLine($"{s1.a}, {s1.b}");
-// StructOp(s1);
-// Console.WriteLine($"{s1.a}, {s1.b}");
+Console.WriteLine("Structs are passed by copy, since they are value types:");
+Console.WriteLine($"{s1.a}, {s1.b}");
+StructOp(s1);
+Console.WriteLine($"{s1.a}, {s1.b}");
 
 // TODO: Create an object instance of a class (which is a reference type)
-
+MyClass cl = new MyClass{a = 5, b = false};
 
 // Perform an operation on the class
 void ClassOp(MyClass theClass) {
@@ -41,10 +46,10 @@ void ClassOp(MyClass theClass) {
     Console.WriteLine($"{theClass.a}, {theClass.b}");
 }
 
-// Console.WriteLine("Objects are passed by reference, since they are reference types:");
-// Console.WriteLine($"{cl.a}, {cl.b}");
-// ClassOp(cl);
-// Console.WriteLine($"{cl.a}, {cl.b}");
+Console.WriteLine("Objects are passed by reference, since they are reference types:");
+Console.WriteLine($"{cl.a}, {cl.b}");
+ClassOp(cl);
+Console.WriteLine($"{cl.a}, {cl.b}");
 
 // These are declared at the bottom of the file because C# requires
 // top-level statements to come before type declarations
--- Start/Strings/Formatting/Program.cs
+++ Start/Strings/Formatting/Program.cs
@@ -5,7 +5,9 @@ float f1 = 123.4f;
 int i1 = 2000;
 
 // TODO: Spacing and Alignment: Indexes
-
+Console.WriteLine("{0, -15} {1, 10}", "Float Val", "Int Val");
+Console.WriteLine("{0, -15} {1, 10}", f1, i1);
 
 // TODO: Spacing and Alignment: Interpolation
-
+Console.WriteLine("{0, -15} {1, 10}", "Float Val", "Int Val");
+Console.WriteLine($"{f1, -15} {i1, 10}");
