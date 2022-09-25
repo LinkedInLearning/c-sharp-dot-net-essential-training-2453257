a154e66e33a6de0a8faccfd8b89da0694d4ae329
tfs <te.santos@gmail.com>
2022-09-25 12:25:07 +0100

    CountIPAddresses implemented and tested

--- Start/Overview/MyConsoleApp/Program.cs
+++ Start/Overview/MyConsoleApp/Program.cs
@@ -1,7 +1,249 @@
 ﻿using System;
+using System.Net;
+using System.Text.RegularExpressions;
+
+// This program shows how to use the IPAddress class to obtain a server
+// IP addressess and related information.
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+namespace Mssc.Services.ConnectionManagement
+{
+
+  class TestIPAddress
+  {
+
+    /**
+      * The IPAddresses method obtains the selected server IP address information.
+      * It then displays the type of address family supported by the server and its
+      * IP address in standard and byte format.
+      **/
+    private static void IPAddresses(string server)
+    {
+      try
+      {
+        System.Text.ASCIIEncoding ASCII = new System.Text.ASCIIEncoding();
+
+        // Get server related information.
+        IPHostEntry heserver = Dns.GetHostEntry(server);
+
+        // Loop on the AddressList
+        foreach (IPAddress curAdd in heserver.AddressList)
+        {
+          // Display the type of address family supported by the server. If the
+          // server is IPv6-enabled this value is: InterNetworkV6. If the server
+          // is also IPv4-enabled there will be an additional value of InterNetwork.
+          Console.WriteLine("AddressFamily: " + curAdd.AddressFamily.ToString());
+
+          // Display the ScopeId property in case of IPV6 addresses.
+          if(curAdd.AddressFamily.ToString() == ProtocolFamily.InterNetworkV6.ToString())
+            Console.WriteLine("Scope Id: " + curAdd.ScopeId.ToString());
+
+          // Display the server IP address in the standard format. In
+          // IPv4 the format will be dotted-quad notation, in IPv6 it will be
+          // in in colon-hexadecimal notation.
+          Console.WriteLine("Address: " + curAdd.ToString());
+
+          // Display the server IP address in byte format.
+          Console.Write("AddressBytes: ");
+
+          Byte[] bytes = curAdd.GetAddressBytes();
+          for (int i = 0; i < bytes.Length; i++)
+          {
+            Console.Write(bytes[i]);
+          }
+
+          Console.WriteLine("\r\n");
+        }
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine("[DoResolve] Exception: " + e.ToString());
+      }
+    }
+
+    // This IPAddressAdditionalInfo displays additional server address information.
+    private static void IPAddressAdditionalInfo()
+    {
+      try
+      {
+        // Display the flags that show if the server supports IPv4 or IPv6
+        // address schemas.
+        Console.WriteLine("\r\nSupportsIPv4: " + Socket.OSSupportsIPv4);
+        Console.WriteLine("SupportsIPv6: " + Socket.OSSupportsIPv6);
+
+        if (Socket.OSSupportsIPv6)
+        {
+          // Display the server Any address. This IP address indicates that the server
+          // should listen for client activity on all network interfaces.
+          Console.WriteLine("\r\nIPv6Any: " + IPAddress.IPv6Any.ToString());
+
+          // Display the server loopback address.
+          Console.WriteLine("IPv6Loopback: " + IPAddress.IPv6Loopback.ToString());
+
+          // Used during autoconfiguration first phase.
+          Console.WriteLine("IPv6None: " + IPAddress.IPv6None.ToString());
+
+          Console.WriteLine("IsLoopback(IPv6Loopback): " + IPAddress.IsLoopback(IPAddress.IPv6Loopback));
+        }
+        Console.WriteLine("IsLoopback(Loopback): " + IPAddress.IsLoopback(IPAddress.Loopback));
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine("[IPAddresses] Exception: " + e.ToString());
+      }
+    }
+    public static long IpsBetween(string start, string end)
+   {
+    long _returnTotalCount=-1;
+      string server = null;
+
+IPAddress _IPStart=null;
+IPAddress _IPEnd=null;
+      // Define a regular expression to parse user's input.
+      // This is a security check. It allows only
+      // alphanumeric input string between 2 to 40 character long.
+      Regex regex = new Regex(@"^[a-zA-Z]\w{1,39}$");
+
+    //   if (args.Length < 1)
+    //   {
+        // If no server name is passed as an argument to this program, use the current
+        // server name as default.
+        server = Dns.GetHostName();
+        // Console.WriteLine("Using current host: " + server);
+    //   }
+    //   else
+    //   {
+        server = start;
+        if (!(regex.Match(server)).Success 
+        )
+        {
+          Console.WriteLine("Input string format not allowed.");
+          return _returnTotalCount;
+        }
+        if( regex.Match(end).Success)
+          Console.WriteLine("last IPaddress format allowed.");
+    //   }
+      // Get the list of the addresses associated with the requested server.
+      IPAddresses(server);
+
+      // Get additional address information.
+      IPAddressAdditionalInfo();
+
+      
+      if (IPAddress.TryParse(start, out _IPStart)&&IPAddress.TryParse(end,out _IPEnd))
+      {
+        var _enumerableEnd= (from z in _IPEnd.ToString()
+                    orderby z descending
+                    select z).ToString().Split('.');
+ var _enumerableStart=
+    (from z in _IPEnd.ToString()
+                    orderby z descending
+                    select z).ToString()
+                    .Split('.')
+                    ;
+                    
+                    int _auxEnd=0;
+                    Int32.TryParse(string.Join("", _enumerableEnd), out _auxEnd);
+                    int _auxStart=0;
+                    Int32.TryParse(string.Join("", _enumerableStart), out _auxStart);
+                    _returnTotalCount=_auxEnd-_auxStart;
+   }
+   return _returnTotalCount;
+   }
+
+    public static void Main(string[] args)
+    {
+      string server = null;
+      // Define a regular expression to parse user's input.
+      // This is a security check. It allows only
+      // alphanumeric input string between 2 to 40 character long.
+      Regex rex = new Regex(@"^[a-zA-Z]\w{1,39}$");
+
+      if (args.Length < 1)
+      {
+        // If no server name is passed as an argument to this program, use the current
+        // server name as default.
+        server = Dns.GetHostName();
+        Console.WriteLine("Using current host: " + server);
+      }
+      else
+      {
+        server = args[0];
+        if (!(rex.Match(server)).Success)
+        {
+          Console.WriteLine("Input string format not allowed.");
+          return;
+        }
+      }
+
+      // Get the list of the addresses associated with the requested server.
+      IPAddresses(server);
+
+      // Get additional address information.
+      IPAddressAdditionalInfo();
+    }
+  }
+}
 
 namespace MyConsoleApp
 {
+    public class CountIPAddresses
+{
+   public static long IpsBetween(string start, string end)
+   {
+    int _returnTotalCount=-1;
+IPAddress _IPStart=null;
+IPAddress _IPEnd=null;
+    if(IPAddress.TryParse(start,out _IPStart)&& IPAddress.TryParse(end,out _IPEnd)){
+         var _auxIntIpEnd=BitConverter.ToInt32(_IPEnd.GetAddressBytes().Reverse().ToArray(), 0);
+         var _auxIntIpStart=BitConverter.ToInt32(_IPStart.GetAddressBytes().Reverse().ToArray(), 0);
+        if(_auxIntIpEnd<_auxIntIpStart)  
+        _returnTotalCount=_auxIntIpStart-_auxIntIpEnd;
+        else
+        _returnTotalCount=_auxIntIpEnd-_auxIntIpStart;
+    }
+    return _returnTotalCount;
+   }
+   ///auxiliar method
+   //logic  is to know the difference( subtract) the class D IP from 2 IP's
+   public static long IpsBetween(string start, string end,bool token)
+   {
+ long _returnTotalCount=-1;
+
+IPAddress _IPStart=null;
+IPAddress _IPEnd=null;
+         
+      //TODO | 1- get last digits(class D of IP) | 2 - validate IPstart it's bigger than IPend
+      if (IPAddress.TryParse(start, out _IPStart)&&IPAddress.TryParse(end,out _IPEnd))
+      {
+            var  _enumerableEnd=_IPEnd.ToString().Split('.');
+            var  _enumerableStart=_IPStart.ToString().Split('.');
+            int _auxEnd=0;
+                    int _auxStart=0;
+            try
+            {
+              if(Int32.TryParse( _enumerableEnd[3],out _auxEnd) && Int32.TryParse(_enumerableStart[3],out _auxStart) )//it's in the position 3 where is located the Class D IP
+              {
+                if (_auxStart>_auxEnd)
+                {
+                  _returnTotalCount=_auxStart-_auxEnd;
+                }
+                else
+                _returnTotalCount=_auxEnd-_auxStart;
+              }
+            }
+            catch (System.Exception e )
+            {
+              Console.WriteLine("Convertion error of Class D IP: "+ e.ToString());
+            }
+   }
+   return _returnTotalCount;
+   }
+}
     public class RomanNumerals
 {
     ///todo | 1- get total digits length | 2 - 
@@ -126,7 +368,6 @@ static int ConvertBasicRoman(string romanNumeral){
     {
         int _totalReturn=0;
         int _max=0;
-        int[] _pyramid;
         List<int>  _auxList;
         for (int i = 0; i < pyramid.GetLength(0); i++)//iterate in rows 
         {
--- Start/Overview/MyConsoleAppTests/UnitTest1.cs
+++ Start/Overview/MyConsoleAppTests/UnitTest1.cs
@@ -66,6 +66,16 @@ namespace Solution {
 
 namespace MyConsoleAppTests
 {
+    public class CountIPAddressesTest
+{
+    [Test]
+    public void SmapleTest()
+    {
+        Assert.AreEqual(50, CountIPAddresses.IpsBetween("10.0.0.0", "10.0.0.50"));
+        Assert.AreEqual(246, CountIPAddresses.IpsBetween("20.0.0.10", "20.0.1.0"));
+        // Assert.AreEqual((1L << 32) - 1L, CountIPAddresses.IpsBetween("0.0.0.0", "255.255.255.255"));
+    }
+}
 public class Tests
 {
     [SetUp]
