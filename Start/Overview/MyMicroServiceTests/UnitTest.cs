c6bdbc773fc10ca6ab277b79e2f4b731806cebdc
tfs <te.santos@gmail.com>
2022-10-04 16:48:46 +0100

    Add Microservice Test project

---
+++ Start/Overview/MyMicroServiceTests/UnitTest.cs
@@ -0,0 +1,14 @@
+
+namespace MyMicrosServiceTests
+{
+    public class MyMicroservice
+    {
+        public void MyTest_DoNothing_EndInNothing()
+        {
+            //assign
+            //action
+            //assert
+            // Assert.AreEqual(0, 0);
+        }
+    }
+}
